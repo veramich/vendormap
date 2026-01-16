49653d94a8ba601fe7a623f07889520c8e583346
Pat <amador.patricia@hotmail.com>
2026-01-16 12:21:55 -0800

    Testing express

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,9 +2,14 @@ import express from 'express';
 
 const app = express();
 
-app.get('/hello', function(req, res) {
-  res.send('Hello, World!');
+app.get('/hello/:name', function(req, res) {
+  res.send('Hello, ' + req.params.name + '!');
 });
+app.use(express.json());
+
+app.post('/hello', function(req, res) {
+  res.send('Hello' + req.body.name + '!');
+})
 
 app.listen(8000, function() {
     console.log('Server is running on port 8000');
