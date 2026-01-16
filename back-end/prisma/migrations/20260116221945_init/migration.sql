9ed1783cefcd2630f93b4f22af222a37988ba639
Pat <amador.patricia@hotmail.com>
2026-01-16 14:25:37 -0800

    Initialized prisma client, previous data was lost

---
+++ back-end/prisma/migrations/20260116221945_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "vendors";
+
+-- CreateTable
+CREATE TABLE "vendors"."vendorlist" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    "location" geometry,
+
+    CONSTRAINT "vendorlist_pkey" PRIMARY KEY ("id")
+);
+
